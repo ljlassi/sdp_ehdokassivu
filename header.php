a793d9d84602904bc46aca5089329e7e923fe892
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-09-16 14:25:07 +0300

    Changed fonts to match SDP visual guidelines

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed:wght@600&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
