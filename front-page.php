9849988a40bb3b9062ef375836fa0196b3d35ca2
Lassi Jääskeläinen <lassijaa@gmail.com>
2020-09-16 14:31:17 +0300

    Working on front page layout and styles.

--- front-page.php
+++ front-page.php
@@ -9,39 +9,28 @@
 <?php get_header(); ?>
 
     <div class="row">
-        <div class="col-md-2 col-sm-0 spacing-sides">
+        <div class="col-md-3 col-sm-0 spacing-sides">
         </div>
-        <div class="col-md-8 col-sm-12 content-main">
+        <div class="col-md-6 col-sm-12 content-main pt-5">
             <div class="row">
-                <div class="col-md-7 col-sm-12">
-                    <div class="main p-3">
-                        <?php
+                <div class="main p-3">
+                    <?php
 
-                        /**
-                         * The post loop, call get the template for displaying page if there is a page to display.
-                         */
+                    /**
+                     * The post loop, call get the template for displaying page if there is a page to display.
+                     */
 
 
-                        if ( have_posts() ) {
-                            while ( have_posts() ) : the_post();
-                                get_template_part( 'template-parts/content/content', 'page' );
-                            endwhile;
-                        }
-                        ?>
-                    </div>
-                </div>
-                <div class="col-md-1 col-sm-0 "></div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="sidebar-right p-3">
-                        <?php if ( is_active_sidebar( 'right-side-bar' ) ) : ?>
-                            <?php dynamic_sidebar( 'right-side-bar' ); ?>
-                        <?php endif; ?>
-                    </div>
+                    if ( have_posts() ) {
+                        while ( have_posts() ) : the_post();
+                            get_template_part( 'template-parts/content/content', 'page' );
+                        endwhile;
+                    }
+                    ?>
                 </div>
             </div>
         </div>
-        <div class="col-md-2 col-sm-0 spacing-sides">
+        <div class="col-md-3 col-sm-0 spacing-sides">
         </div>
-
     </div>
 <?php get_footer(); ?>
